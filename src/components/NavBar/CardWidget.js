aa8677d535291870e069878045e14f0ba525aa0a
Juan Carreras <jc@adhoc.com.ar>
2021-10-04 21:47:39 -0300

    Entrega 11

--- src/components/NavBar/CardWidget.js
+++ src/components/NavBar/CardWidget.js
@@ -1,9 +1,32 @@
+import { useCart } from "../../context/CartContext";
+
 function ShoppingCart() {
+    const { cart } = useCart();
+    let itemCount = 0;
+
+    {
+        cart?.map((item) => {
+            itemCount += item.qty;
+        })
+    };
+
     return (
-        <div className="App ml-auto">
-        <button className="fa fa-shopping-cart" aria-hidden="true"/>
+        <div>
+            {itemCount === 0 && (<div className="cart-badge"><span id="cart-count"><i className="fas fa-shopping-cart"></i></span></div>)}
+            {itemCount > 0 && (
+                <div className="cart-badge"> 
+                    <span className="d-flex flex-row">
+                        <i className="fas fa-shopping-cart"></i>
+                        <span className="itemCount">
+                            <h6 className="number">{itemCount}</h6>
+                        </span>
+                    </span>
+                </div>
+
+            )}
+
         </div>
-    );
-  }
+    )
+};
 
 export default ShoppingCart;
